d7ae570d66392af3b1e84e262b8c42de8694c874
Aaron <89910093+minrazen@users.noreply.github.com>
2026-03-27 12:04:45 +0100

    Update SwitchMove.cs

--- Assets/Scripts/SwitchMove.cs
+++ Assets/Scripts/SwitchMove.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SwitchMove", menuName = "Create/SwitchMove")]
 public class SwitchMove : CharacterMove
 {
-    public override void Execute(BattleCharacter user, BattleCharacter target)
+    public override void Execute(BattleCharacter user, BattleCharacter _)
     {
         user.playerOwner.PerformSwitch(user.queuedSwitchIndex);
         Debug.Log($"{user.gameObject.name} retreated!");
